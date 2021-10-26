38a0957dd6fdf5f45a7d5d90ab1d767e74ce0e68
noshluk2 <noshluk2@gmail.com>
2021-10-26 14:19:06 +0500

    multi Robot Remaining

--- autonomous_robots/scripts/go_to_goal.py
+++ autonomous_robots/scripts/go_to_goal.py
@@ -4,18 +4,18 @@
 import rospy
 import actionlib
 from move_base_msgs.msg import MoveBaseAction, MoveBaseGoal
-
+import sys
 def movebase_client():
 
     client = actionlib.SimpleActionClient('move_base',MoveBaseAction)
     client.wait_for_server()
-
+    argv = sys.argv[1:]
     goal = MoveBaseGoal()
     goal.target_pose.header.frame_id = "map"
     goal.target_pose.header.stamp = rospy.Time.now()
-    goal.target_pose.pose.position.x = 3.6014963
-    goal.target_pose.pose.position.y = -1.156947665
-    goal.target_pose.pose.position.z = -0.00100955
+    goal.target_pose.pose.position.x = float(argv[0])
+    goal.target_pose.pose.position.y = float(argv[1])
+    goal.target_pose.pose.position.z = 0.000
 
     goal.target_pose.pose.orientation.w = 0.19012465928
     goal.target_pose.pose.orientation.z = 0.998917586
